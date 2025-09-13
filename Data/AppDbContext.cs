422185588882c59e909ade9c7971030442714b66
Breno Alves <biomassa141@gmail.com>
2025-09-13 18:54:42 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ApiWebKut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWebKut.Data
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
